65fc7b4ece3a853c084c6c88db031233d1b123ae
Andriy Klitsuk <andriy@klits.uk>
2020-07-21 10:40:34 +0200

    feat(header): social links in header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,53 +1,70 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
+import useScrollPosition from '@react-hook/window-scroll'
 
 import WsLogo from '../../images/logo_wes.svg'
-import menuItems from './menu-items'
 
 import './styles.scss'
 import 'animate.css/animate.min.css'
 import MenuItem from './MenuItem'
 
-const Header = ({ siteTitle }) => (
-    <header className="main-header">
-        <div className="container">
-            <div className="logo-wrapper">
-                <img src={WsLogo} alt="WeStudents logo" />
-            </div>
-            <div className="menu-wrapper">
-                <nav>
-                    <ul className="navigation">
-                        {menuItems.map((item) => (
-                            <MenuItem key={item.key} item={item} />
-                        ))}
-                    </ul>
-                </nav>
-            </div>
-            <div className="socials-wrapper">{/* Social links */}</div>
-        </div>
+import menuItems from './menu-items'
+import socialItems from './social-items'
+import interpolate from '../../utils/interpolate'
 
-        <div
+const Header = ({ siteTitle }) => {
+    const [shadowOpacity, setShadowOpacity] = useState(0)
+    const scrollY = useScrollPosition(60 /* fps */)
+    const interpolator = useMemo(() =>
+        interpolate({
+            inputRange: [0, 300],
+            outputRange: [0, 9],
+            clamp: true,
+        }),
+    )
+
+    useEffect(() => {
+        const so = Math.ceil(interpolator(scrollY > 300 ? 300 : scrollY))
+        setShadowOpacity(so)
+    }, [scrollY])
+
+    return (
+        <header
+            className="main-header"
             style={{
-                margin: `0 auto`,
-                maxWidth: 960,
-                padding: `1.45rem 1.0875rem`,
+                boxShadow: `0 14px 28px rgba(0,0,0,0.0${shadowOpacity}), 0 10px 10px rgba(0,0,0,0.0${shadowOpacity})`,
             }}
         >
-            <h1 style={{ margin: 0 }}>
-                <Link
-                    to="/"
-                    style={{
-                        color: `white`,
-                        textDecoration: `none`,
-                    }}
-                >
-                    {siteTitle}
-                </Link>
-            </h1>
-        </div>
-    </header>
-)
+            <div className="container">
+                <div className="logo-wrapper">
+                    <img src={WsLogo} alt="WeStudents logo" />
+                </div>
+                <div className="menu-wrapper">
+                    <nav>
+                        <ul className="navigation">
+                            {menuItems.map((item) => (
+                                <MenuItem key={item.key} item={item} />
+                            ))}
+                        </ul>
+                    </nav>
+                </div>
+                <div className="socials-wrapper">
+                    {socialItems.map((item) => (
+                        <a
+                            href={item.href}
+                            target="_blank"
+                            rel="noreferrer"
+                            className="social-icon-wrapper"
+                        >
+                            <img src={item.image} alt={`${item.value} logo`} />
+                        </a>
+                    ))}
+                </div>
+            </div>
+        </header>
+    )
+}
 
 Header.propTypes = {
     siteTitle: PropTypes.string,
---
+++ src/components/header/social-items.js
@@ -0,0 +1,11 @@
+import InstagramLogo from '../../images/instagram_icon.svg'
+import FacebookLogo from '../../images/facebook_icon.svg'
+import LinkedinLogo from '../../images/linkedin_icon.svg'
+import TiktokLogo from '../../images/tiktok_icon.svg'
+
+export default [
+    { key: 'team', image: InstagramLogo, href: 'https://instagram.com' },
+    { key: 'careers', image: FacebookLogo, href: 'https://facebook.com' },
+    { key: 'community', image: LinkedinLogo, href: 'https://linkedin.com' },
+    { key: 'partners', image: TiktokLogo, href: 'https://tiktok.com' },
+]
