be77f18b15e84664ccd37bc30440c812acce8556
ndrkltsk <aklitsuk@gmail.com>
2020-07-21 17:08:57 +0200

    feat(homepage wip): homepage westudentsapp and wegather sections

---
+++ src/components/lotties/main/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Lottie from 'react-lottie'
+
+import * as animationData from './data.json'
+
+const MainLottieAnimation = ({ width, height }) => {
+    return (
+        <Lottie
+            options={{
+                loop: true,
+                autoplay: true,
+                animationData: animationData.default,
+                rendererSettings: {
+                    preserveAspectRatio: 'xMidYMid slice',
+                },
+            }}
+            height={height}
+            width={width}
+            isStopped={false}
+            isPaused={false}
+        />
+    )
+}
+
+export default MainLottieAnimation
