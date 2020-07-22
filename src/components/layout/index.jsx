c4e93b9d59f3b94e5a4eca43e1514b63a947748f
Andriy Klitsuk <andriy@klits.uk>
2020-07-22 16:15:37 +0200

    feat(layout): updated layout to contain fullpage library with section prop

---
+++ src/components/layout/index.jsx
@@ -0,0 +1,75 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import ReactFullpage from '@fullpage/react-fullpage'
+
+import Bubbles from '../bubbles/index'
+import SEO from '../seo'
+
+import Header from '../header'
+import './layout.scss'
+
+const Layout = ({ className, sections, seo, showBubbles }) => {
+    const data = useStaticQuery(graphql`
+        query SiteTitleQuery {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
+        }
+    `)
+
+    return (
+        <div className={className}>
+            <SEO {...seo} />
+            <Header siteTitle={data.site.siteMetadata.title} />
+            <div style={{ margin: `0 auto` }}>
+                <ReactFullpage
+                    anchors={['intro', 'westudents-app']}
+                    navigation
+                    scrollingSpeed={1000} /* Options here */
+                    autoScrolling
+                    scrollHorizontally
+                    scrollOverflow
+                    scrollOverflowReset
+                    scrollOverflowOptions={{ scrollbars: false }}
+                    scrollBar={false}
+                    onLeave={(origin, destination, direction) => {
+                        document.dispatchEvent(
+                            new CustomEvent('section-scrolled', {
+                                detail: {
+                                    origin,
+                                    destination,
+                                    direction,
+                                },
+                            }),
+                        )
+                    }}
+                    render={(fullpageProps) => (
+                        <>
+                            {showBubbles ? (
+                                <Bubbles sectionCount={fullpageProps.state.sectionCount} />
+                            ) : null}
+                            {sections.map((renderSection) => (
+                                <div className="section">{renderSection(fullpageProps)}</div>
+                            ))}
+                            <div className="section">
+                                <footer>© {new Date().getFullYear()}, Built with Mario</footer>
+                            </div>
+                        </>
+                    )}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Layout
