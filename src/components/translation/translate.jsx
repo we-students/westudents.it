5f4f170c9ca3a5a9125c9ee97e39e9986d5c70db
Andriy Klitsuk <andriy@klits.uk>
2020-07-23 16:38:37 +0200

    feat(contentful): contentful configuration + translation component update

--- src/components/translation/translate.jsx
+++ src/components/translation/translate.jsx
@@ -1,13 +1,32 @@
 import i18n from 'i18n-js'
-import itTranslation from './it.json'
+import { useStaticQuery, graphql } from 'gatsby'
 
 const Translate = (props) => {
     const { config, children } = props
-    i18n.defaultLocale = 'it-IT'
-    i18n.locale = 'it-IT'
 
-    i18n.translations['it-IT'] = itTranslation
-    return i18n.t(children, config)
+    const data = useStaticQuery(graphql`
+        query LocaleQuery {
+            contentfulLocale(locale: { eq: "it-IT" }) {
+                children {
+                    internal {
+                        content
+                    }
+                }
+            }
+        }
+    `)
+
+    if (data.contentfulLocale === null) return `${children} - NO LOCALE`
+
+    try {
+        const translation = data.contentfulLocale.children[0].internal.content
+        i18n.defaultLocale = 'it-IT'
+        i18n.locale = 'it-IT'
+        i18n.translations['it-IT'] = JSON.parse(translation)
+        return i18n.t(children, config)
+    } catch (err) {
+        return `${children} - NO TRANSLATION`
+    }
 }
 
 export default Translate
