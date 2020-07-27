814dcf800ea0cf8884247e37bbbe440ed93c3e81
ndrkltsk <aklitsuk@gmail.com>
2020-07-27 11:15:13 +0200

    feat(careers wip): careers page

---
+++ src/pages/careers/sections/DevTeam.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import DevShitImageOne from '../../../images/dev-team-components/1.png'
+import DevShitImageTwo from '../../../images/dev-team-components/2.png'
+import DevShitImageThree from '../../../images/dev-team-components/3.png'
+import DevShitImageFour from '../../../images/dev-team-components/4.png'
+import DevShitImageFive from '../../../images/dev-team-components/5.png'
+import DevShitImageSix from '../../../images/dev-team-components/6.png'
+import DevShitImageSeven from '../../../images/dev-team-components/7.png'
+
+const OpenPosition = ({ fullpageProps }) => {
+    const handleCtaClick = () => {}
+
+    return (
+        <div className={`dev-team ${fullpageProps.isActive ? 'active' : ''}`}>
+            <div className="container">
+                <div className="dev-things">
+                    <img alt="Dev thing" src={DevShitImageOne} />
+                    <img alt="Dev thing" src={DevShitImageTwo} />
+                    <img alt="Dev thing" src={DevShitImageThree} />
+                    <img alt="Dev thing" src={DevShitImageFour} />
+                    <img alt="Dev thing" src={DevShitImageFive} />
+                    <img alt="Dev thing" src={DevShitImageSix} />
+                    <img alt="Dev thing" src={DevShitImageSeven} />
+                    <img alt="Dev thing" src={DevShitImageSeven} />
+                </div>
+                <div>
+                    <button type="button" onClick={handleCtaClick}>
+                        Diventa un developer
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default OpenPosition
---
+++ src/pages/careers/sections/OpenPositions.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+
+import { BLOCKS, MARKS } from '@contentful/rich-text-types'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+
+import GridList from '../../../components/grid-list'
+
+const Bold = ({ children }) => <span className="bold">{children}</span>
+const Text = ({ children }) => <p className="align-center">{children}</p>
+
+const options = {
+    renderMark: {
+        [MARKS.BOLD]: (text) => <Bold>{text}</Bold>,
+    },
+    renderNode: {
+        [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+    },
+}
+
+const OpenPosition = ({ fullpageProps }) => {
+    const data = useStaticQuery(graphql`
+        query OpenPositionsQuery {
+            allContentfulOpenPosition {
+                edges {
+                    node {
+                        category
+                        level
+                        place
+                        title
+                        createdAt(formatString: "DD/MM/YYYY")
+                        description {
+                            json
+                        }
+                        shortDescription {
+                            json
+                        }
+                        icon {
+                            fluid {
+                                srcSet
+                                srcSetWebp
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    `).allContentfulOpenPosition.edges.map(({ node }) => node)
+
+    return (
+        <div className="open-positions container">
+            <h2>Tutte le posizioni aperte</h2>
+
+            <p>Non sei un developer o un designer? Scopri le altre posizioni aperte!</p>
+            <GridList
+                items={data}
+                cols={3}
+                renderItem={(item, index) => {
+                    return (
+                        <div key={index} className="grid-item open-position-item">
+                            <img
+                                srcSet={item.icon.fluid.srcSet}
+                                srcSetWebp={item.icon.fluid.srcSetWebp}
+                                alt="Posizione aperta"
+                            />
+                            <h4>{item.title}</h4>
+                            <p>{documentToReactComponents(item.shortDescription.json, options)} </p>
+                        </div>
+                    )
+                }}
+            />
+        </div>
+    )
+}
+
+export default OpenPosition
+
+//             {data.map((node) => documentToReactComponents(node.description.json, options))}
