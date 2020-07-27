a9c335b91349697688630c064e861d4644cec1d1
MarioF-dev <mariolorenzo.francese@gmail.com>
2020-07-27 17:48:51 +0200

    feat(feature/community-page): community page

--- src/components/videobg-wrapper/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import './styles.scss'
-
-const BGWrapper = ({ children }) => {
-    return <div className="video-intro-wrapper">{children}</div>
-}
-
-export default BGWrapper
--- src/pages/community/index.jsx
+++ src/pages/community/index.jsx
@@ -6,6 +6,9 @@ import Education from './Education'
 import Connection from './Connection'
 import Freedom from './Freedom'
 import Features from './Features'
+import Ambassador from './Ambassador'
+import Representant from './Representant'
+import Team from './Team'
 
 import './styles.scss'
 
@@ -13,17 +16,43 @@ const pageTitle = 'WeStudents — Community'
 
 const CommunityPage = () => (
     <Layout
-        showFooter
         className="community-wrapper"
         seo={{ title: pageTitle }}
         showBubbles
+        showFooter={false}
         sections={[
-            (fullpageProps) => <Intro fullpageProps={fullpageProps} />,
-            (fullpageProps) => <Education fullpageProps={fullpageProps} />,
-            (fullpageProps) => <Freedom fullpageProps={fullpageProps} />,
-            (fullpageProps) => <Connection fullpageProps={fullpageProps} />,
-            (fullpageProps) => <Features fullpageProps={fullpageProps} />,
-            (fullpageProps) => <p>Prova</p>,
+            {
+                anchor: 'intro',
+                render: (fullpageProps) => <Intro fullpageProps={fullpageProps} />,
+            },
+            {
+                anchor: 'Education',
+                render: (fullpageProps) => <Education fullpageProps={fullpageProps} />,
+            },
+            {
+                anchor: 'Freedom',
+                render: (fullpageProps) => <Freedom fullpageProps={fullpageProps} />,
+            },
+            {
+                anchor: 'Connection',
+                render: (fullpageProps) => <Connection fullpageProps={fullpageProps} />,
+            },
+            {
+                anchor: 'Features',
+                render: (fullpageProps) => <Features fullpageProps={fullpageProps} />,
+            },
+            {
+                anchor: 'Ambassador',
+                render: (fullpageProps) => <Ambassador fullpageProps={fullpageProps} />,
+            },
+            {
+                anchor: 'Representant',
+                render: (fullpageProps) => <Representant fullpageProps={fullpageProps} />,
+            },
+            {
+                anchor: 'Team',
+                render: (fullpageProps) => <Team fullpageProps={fullpageProps} />,
+            },
         ]}
     />
 )
