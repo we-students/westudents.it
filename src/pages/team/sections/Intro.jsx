5fab5111e22720dc0988721e47f7fe757e8b58e3
MarioF-dev <mariolorenzo.francese@gmail.com>
2020-07-30 16:00:03 +0200

    feat(feature/team): team page

--- src/pages/team/sections/Intro.jsx
+++ src/pages/team/sections/Intro.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useState, useEffect } from 'react'
+import Translate from '../../../components/translation/translate'
 
 import '../styles.scss'
 
@@ -15,16 +16,38 @@ const Intro = () => {
 
     return (
         <div className={`intro ${fullScreen ? 'fullscreen' : ''}`}>
-            <div className="image-section">
-                <div
-                    className="team-image"
-                    onClick={() => {
-                        setFullscreen(!fullScreen)
-                    }}
-                />
+            <div className="intro-row">
+                <div className="image-section">
+                    <div
+                        className="team-image"
+                        onClick={() => {
+                            setFullscreen(!fullScreen)
+                        }}
+                    />
+                </div>
+                <div className="text-section">
+                    <div>
+                        <p className="title">
+                            <Translate>TEAM.INTRO.MEET</Translate>
+                        </p>
+                        <h1 className="title-big">
+                            <Translate>TEAM.INTRO.TEAM</Translate>
+                        </h1>
+                        <div>
+                            <p>
+                                <Translate>TEAM.INTRO.DESCRIPTION1</Translate>
+                            </p>
+                            <p>
+                                <Translate>TEAM.INTRO.DESCRIPTION2</Translate>
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="text-section">
-                <h2>test</h2>
+            <div className="bottom-description">
+                <p>
+                    <Translate>TEAM.INTRO.DISCOVER</Translate>
+                </p>
             </div>
         </div>
     )
---
+++ src/pages/team/sections/TeamProfiles.jsx
@@ -0,0 +1,109 @@
+/* eslint-disable global-require */
+/* eslint-disable react/no-array-index-key */
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import GridList from '../../../components/grid-list'
+
+const Profile = (props) => {
+    const { name, image, activeImage, animImages, role } = props
+    return (
+        <div className="profile-item">
+            <div>
+                <img
+                    srcSet={image.fluid.srcSet}
+                    srcSetWebp={image.fluid.srcSetWebp}
+                    alt="WeStudents"
+                    className="main-image"
+                />
+                <img
+                    srcSet={activeImage.fluid.srcSet}
+                    srcSetWebp={activeImage.fluid.srcSetWebp}
+                    alt="WeStudents"
+                    className="image active-image"
+                />
+                <img
+                    src={require('../../../images/profile_fade.png')}
+                    className="image profile-fade"
+                    alt="WeStudents"
+                />
+                {animImages
+                    ? animImages.map((item, index) => (
+                          <img
+                              srcSet={item.fluid.srcSet}
+                              srcSetWebp={item.fluid.srcSetWebp}
+                              alt="WeStudents"
+                              className={`image anim-image animation_${index}`}
+                          />
+                      ))
+                    : null}
+                <div className="text">
+                    <h4>{name}</h4>
+                    <p>{role}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const TeamProfiles = () => {
+    const data = useStaticQuery(graphql`
+        query PersonQuery {
+            allContentfulPerson {
+                edges {
+                    node {
+                        name
+                        role
+                        image {
+                            fluid {
+                                srcSet
+                                srcSetWebp
+                            }
+                        }
+                        activeImage {
+                            fluid {
+                                srcSet
+                                srcSetWebp
+                            }
+                        }
+                        animImages {
+                            fluid {
+                                srcSet
+                                srcSetWebp
+                            }
+                        }
+                        order
+                    }
+                }
+            }
+        }
+    `)
+        .allContentfulPerson.edges.map(({ node }) => node)
+        .sort((a, b) => {
+            if (a.order > b.order) {
+                return 1
+            }
+            if (b.order > a.order) {
+                return -1
+            }
+            return 0
+        })
+    return (
+        <div className="container team">
+            <GridList
+                items={data}
+                cols={3}
+                renderItem={(item) => (
+                    <Profile
+                        role={item.role}
+                        image={item.image}
+                        activeImage={item.activeImage}
+                        name={item.name}
+                        animImages={item.animImages}
+                    />
+                )}
+            />
+        </div>
+    )
+}
+
+export default TeamProfiles
