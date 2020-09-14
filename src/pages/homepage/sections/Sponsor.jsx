22c49eccd85ed15918542d70b851ed1c060ab279
ndrkltsk <aklitsuk@gmail.com>
2020-09-14 17:16:36 +0200

    feat: partners on contentful

--- src/pages/homepage/sections/Sponsor.jsx
+++ src/pages/homepage/sections/Sponsor.jsx
@@ -1,32 +1,40 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 import Translate from '../../../components/translation/translate'
 
-const GomaIcon = require('../../../images/sponsors/Goma.svg')
-const MediolanumIcon = require('../../../images/sponsors/Mediolanum.svg')
-const MuseoegizioIcon = require('../../../images/sponsors/Museo_egizio.svg')
-const PoloIcon = require('../../../images/sponsors/Polo_del_900.svg')
-const YoungIcon = require('../../../images/sponsors/Young_platform.svg')
-
-const sponsors = [
-    { name: 'Goma elettronica', icon: GomaIcon },
-    { name: 'Mediolanum', icon: MediolanumIcon },
-    { name: 'young platform', icon: YoungIcon },
-    { name: 'museo egizio', icon: MuseoegizioIcon },
-    { name: 'polo del 900', icon: PoloIcon },
-]
-
 const Sponsor = () => {
+    const partners = useStaticQuery(graphql`
+        query {
+            allContentfulPartner {
+                nodes {
+                    nome
+                    order
+                    logo {
+                        file {
+                            url
+                        }
+                    }
+                }
+            }
+        }
+    `)
+        .allContentfulPartner.nodes.map((node) => ({
+            name: node.nome,
+            img: node.logo.file.url,
+            order: node.order,
+        }))
+        .sort((a, b) => a.order - b.order)
+
     return (
         <div className="sponsor container d-flex">
             <h1 className="title text-center">
                 <Translate>HOMEPAGE.SPONSORS.TITLE</Translate>
             </h1>
             <div className="sponsors-items">
-                {sponsors.map((item) => (
+                {partners.map((item) => (
                     <img
                         key={`img_${item.name}`}
-                        src={item.icon}
+                        src={item.img}
                         alt={item.name}
                         className="sponsor-icon"
                     />
--- src/pages/products/westudents/sections/Sponsor.jsx
+++ src/pages/products/westudents/sections/Sponsor.jsx
@@ -1,33 +1,40 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { graphql, Link, useStaticQuery } from 'gatsby'
 
 import Translate from '../../../../components/translation/translate'
 
-const GomaIcon = require('../../../../images/sponsors/Goma.svg')
-const MediolanumIcon = require('../../../../images/sponsors/Mediolanum.svg')
-const MuseoegizioIcon = require('../../../../images/sponsors/Museo_egizio.svg')
-const PoloIcon = require('../../../../images/sponsors/Polo_del_900.svg')
-const YoungIcon = require('../../../../images/sponsors/Young_platform.svg')
-
-const sponsors = [
-    { name: 'Goma elettronica', icon: GomaIcon },
-    { name: 'Mediolanum', icon: MediolanumIcon },
-    { name: 'young platform', icon: YoungIcon },
-    { name: 'museo egizio', icon: MuseoegizioIcon },
-    { name: 'polo del 900', icon: PoloIcon },
-]
-
 const Sponsor = () => {
+    const partners = useStaticQuery(graphql`
+        query {
+            allContentfulPartner {
+                nodes {
+                    nome
+                    order
+                    logo {
+                        file {
+                            url
+                        }
+                    }
+                }
+            }
+        }
+    `)
+        .allContentfulPartner.nodes.sort((a, b) => a.order - b.order)
+        .map((node) => ({
+            name: node.nome,
+            img: node.logo.file.url,
+        }))
+
     return (
         <div className="sponsor container d-flex">
             <h1 className="title text-center">
                 <Translate>HOMEPAGE.SPONSORS.TITLE</Translate>
             </h1>
             <div className="sponsors-items">
-                {sponsors.map((item) => (
+                {partners.map((item) => (
                     <img
                         key={`img_${item.name}`}
-                        src={item.icon}
+                        src={item.img}
                         alt={item.name}
                         className="sponsor-icon"
                     />
